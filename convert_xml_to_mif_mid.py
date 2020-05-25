7f7aea65310bbafadeab8449b596c4fe3dd6df52
sladkogolik901 <79827134305@yandex.ru>
2020-05-25 20:29:15 +0500

    Version 2

--- convert_xml_to_mif_mid.py
+++ convert_xml_to_mif_mid.py
@@ -5,7 +5,6 @@ from xml.etree import ElementTree as ET
 #Variables for save coords
 coord_multi = []
 coord_one = []
-coord_one_copy = []
 
 mif_file = open('response.mif', 'w', encoding='cp1251')
 mid_file = open('response.mid', 'w', encoding='cp1251')
@@ -31,59 +30,61 @@ for event, elem in xml_iter:
         elif elem.tag == 'land_records':
             land_records = 1
 # Закрывающий тэг, доступны узлы-потомки, текстовые узлы
-    if event == 'end':
-# Сохраняем в переменную кадастровый номер
-        if elem.tag == 'cad_number':
-            cad_number = text
-# Объект закончился, данные по нему считаны, записываем в файл
-#----------------------------------------------------------------------------------------
-        elif elem.tag == 'contours' or elem.tag == 'cadastral_blocks':
-            conturs = 0
-            print('writing')
-            # Проверка, что данные не пустые
-            if len(coord_one_copy) > 0:
-                mif_file.write(' ' + 'Region ')
-                # Число участков в контуре
-                mif_file.write(str(len(coord_multi)) + '\n')
-                # Кадастровый номер
-                mid_file.write('"' + cad_number + '"' + '\n')
-                for items in coord_multi:
-                    if (len(items)) > 0:
-                        # Записываем число координат
-                        # Деление пополам - т.к в списке обе координаты x и y
-                        mif_file.write(str(len(items)//2) + '\n')
-                        # Цикл записи координат x и y, x занимает нечетную позицию в списке,y - четную
-                        i = 0
-                        while i < len(items):
-                            mif_file.write(items[i])
-                            mif_file.write(' ')
-                            mif_file.write(items[i+1])
-                            mif_file.write('\n')
-                            i += 2
-                        mif_file.write('\n')
-                # Очищаем массив для последующих данных
-                coord_multi.clear()
-        # Удаляем элементы прочитанной части дерева
-        elem.clear()
-#----------------------------------------------------------------------------------------
-# Признак того, что элементы контура закончились
-# Перезаписываем координаты в список для записи coord_multi
-        if elem.tag == 'contour':
-            contur = 0
-            coord_one_copy = coord_one.copy()
-            coord_multi.append(coord_one_copy)
-            coord_one.clear()
-# Сохраняем в список координаты одного контура
-        if land_records and contur:
-            if elem.tag == 'x':
-                coord_one.append(text)
-            elif elem.tag == 'y':
-                coord_one.append(text)
-#--------------------------------------
+    elif event == 'end':
 # Слой КПТ_Участки завершен
-        elif elem.tag == 'land_records':
+        if elem.tag == 'land_records':
             land_records = 0
+        if land_records:
+            # Сохраняем в переменную кадастровый номер
+            if elem.tag == 'cad_number':
+                cad_number = text
+    # Объект закончился, данные по нему считаны, записываем в файл
+    #----------------------------------------------------------------------------------------
+    #        elif elem.tag == 'contours' or elem.tag == 'cadastral_blocks':
+            elif elem.tag == 'contours':
+                conturs = 0
+                print('writing')
+                # Проверка, что данные не пустые
+                if len(coord_multi) > 0:
+                    mif_file.write(' ' + 'Region ')
+                    # Число участков в контуре
+                    mif_file.write(str(len(coord_multi)) + '\n')
+                    # Кадастровый номер
+                    mid_file.write('"' + cad_number + '"' + '\n')
+                    for items in coord_multi:
+                        if (len(items)) > 0:
+                            # Записываем число координат
+                            # Деление пополам - т.к в списке обе координаты x и y
+                            mif_file.write(str(len(items)//2) + '\n')
+                            # Цикл записи координат x и y, x занимает нечетную позицию в списке,y - четную
+                            i = 0
+                            while i < len(items):
+                                mif_file.write(items[i])
+                                mif_file.write(' ')
+                                mif_file.write(items[i+1])
+                                mif_file.write('\n')
+                                i += 2
+                            mif_file.write('\n')
+                    # Очищаем массив для последующих данных
+                    coord_multi.clear()
+                # Удаляем элементы прочитанной части дерева
+                elem.clear()
+    #----------------------------------------------------------------------------------------
+    # Признак того, что элементы контура закончились
+    # Перезаписываем координаты в список для записи coord_multi
+            elif elem.tag == 'contour':
+                contur = 0
+                if len(coord_one) > 0:
+                    coord_multi.append(coord_one)
+                    coord_one = []
+    # Сохраняем в список координаты одного контура
+            if contur:
+                if elem.tag == 'x':
+                    coord_one.append(text)
+                elif elem.tag == 'y':
+                    coord_one.append(text)
+#--------------------------------------
+
 
 mif_file.close()
 mid_file.close()
-
