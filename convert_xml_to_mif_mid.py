63b03e12d9d69d56c68b4ee5719e4f53cac847b8
sladkogolik901 <79827134305@yandex.ru>
2020-05-25 14:04:10 +0500

    Add files
    Work for one layer - КПТ_Участки

---
+++ convert_xml_to_mif_mid.py
@@ -0,0 +1,89 @@
+#Последний рабочий вариант
+
+from xml.etree import ElementTree as ET
+
+#Variables for save coords
+coord_multi = []
+coord_one = []
+coord_one_copy = []
+
+mif_file = open('response.mif', 'w', encoding='cp1251')
+mid_file = open('response.mid', 'w', encoding='cp1251')
+str_heading = 'Version 300' + '\n' + 'Charset "WindowsCyrillic"' + '\n' + \
+               'Delimiter ","' + '\n' + 'CoordSys NonEarth Units "m" Bounds (200000, 2000000) (800000, 2500000)' + \
+               '\n' + 'Columns 1' + '\n' + '  ' + 'КН Char(30)' + '\n' + 'Data' + 2*'\n'
+# Flags
+contur = 0
+conturs = 0
+land_records = 0
+
+mif_file.write(str_heading)
+
+xml_iter = ET.iterparse('Response №50-27939031.xml', events=('start', 'end'))
+for event, elem in xml_iter:
+    text = elem.text
+# Открывающий тэг, данные потомка и элемента пока недоступны
+    if event == 'start':
+# Признак начала объекта (одноконтурный или часть многоконтурного)
+        if elem.tag == 'contour':
+            contur = 1
+# Признак КПТ_УЧАСТКИ
+        elif elem.tag == 'land_records':
+            land_records = 1
+# Закрывающий тэг, доступны узлы-потомки, текстовые узлы
+    if event == 'end':
+# Сохраняем в переменную кадастровый номер
+        if elem.tag == 'cad_number':
+            cad_number = text
+# Объект закончился, данные по нему считаны, записываем в файл
+#----------------------------------------------------------------------------------------
+        elif elem.tag == 'contours' or elem.tag == 'cadastral_blocks':
+            conturs = 0
+            print('writing')
+            # Проверка, что данные не пустые
+            if len(coord_one_copy) > 0:
+                mif_file.write(' ' + 'Region ')
+                # Число участков в контуре
+                mif_file.write(str(len(coord_multi)) + '\n')
+                # Кадастровый номер
+                mid_file.write('"' + cad_number + '"' + '\n')
+                for items in coord_multi:
+                    if (len(items)) > 0:
+                        # Записываем число координат
+                        # Деление пополам - т.к в списке обе координаты x и y
+                        mif_file.write(str(len(items)//2) + '\n')
+                        # Цикл записи координат x и y, x занимает нечетную позицию в списке,y - четную
+                        i = 0
+                        while i < len(items):
+                            mif_file.write(items[i])
+                            mif_file.write(' ')
+                            mif_file.write(items[i+1])
+                            mif_file.write('\n')
+                            i += 2
+                        mif_file.write('\n')
+                # Очищаем массив для последующих данных
+                coord_multi.clear()
+        # Удаляем элементы прочитанной части дерева
+        elem.clear()
+#----------------------------------------------------------------------------------------
+# Признак того, что элементы контура закончились
+# Перезаписываем координаты в список для записи coord_multi
+        if elem.tag == 'contour':
+            contur = 0
+            coord_one_copy = coord_one.copy()
+            coord_multi.append(coord_one_copy)
+            coord_one.clear()
+# Сохраняем в список координаты одного контура
+        if land_records and contur:
+            if elem.tag == 'x':
+                coord_one.append(text)
+            elif elem.tag == 'y':
+                coord_one.append(text)
+#--------------------------------------
+# Слой КПТ_Участки завершен
+        elif elem.tag == 'land_records':
+            land_records = 0
+
+mif_file.close()
+mid_file.close()
+
